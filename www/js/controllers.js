cbddb0c04220cda65a5212d5b520785c30ac1624
JohnGwyneth <johngwyneth@hotmail.com>
2017-01-24 16:01:45 +0000

    Front End Changes - Need to now add navigation buttons

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -81,13 +81,20 @@ function($scope, $stateParams, $window, stockDataService, dateService, chartData
   };
 
   function getPriceData() {
-    var promise = stockDataService.getPriceData($scope.ticker);
 
-    promise.then(function(data){
-      console.log(data);
-      $scope.stockPriceData = data;
-    });
-  }
+      var promise = stockDataService.getPriceData($scope.ticker);
+
+      promise.then(function(data) {
+        $scope.stockPriceData = data;
+
+        if(data.chg_percent >= 0 && data !== null) {
+          $scope.reactiveColor = {'background-color': '#33cd5f', 'border-color': 'rgba(255,255,255,.3)'};
+        }
+        else if(data.chg_percent < 0 && data !== null) {
+          $scope.reactiveColor = {'background-color' : '#ef473a', 'border-color': 'rgba(0,0,0,.2)'};
+        }
+      });
+    }
 
   function getDetailsData() {
     var promise = stockDataService.getDetailsData($scope.ticker);
@@ -149,7 +156,7 @@ function getChartData() {
   	$scope.chartOptions = {
       chartType: 'linePlusBarWithFocusChart',
       data: 'myData',
-      margin: {top: 15, right: 40, bottom: marginBottom, left: 70},
+      margin: {top: 15, right: 0, bottom: marginBottom, left: 0},
       interpolate: "cardinal",
       useInteractiveGuideline: false,
       yShowMaxMin: false,
@@ -164,7 +171,10 @@ function getChartData() {
       y2AxisTickFormat: y2TickFormat,
       y3AxisTickFormat: y3TickFormat,
       y4AxisTickFormat: y4TickFormat,
-      transitionDuration: 500
+      transitionDuration: 500,
+      y1AxisLabel: 'Price',
+      y3AxisLabel: 'Volume',
+      noData: 'Loading Data...'
   	};
 
 }]);
